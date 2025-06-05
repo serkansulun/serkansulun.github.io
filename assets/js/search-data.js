bb290f9bc0154e9d619fe3ee52306e595d028a1e
serkansulun <serkan.sulun@inesctec.pt>
2025-06-05 16:32:12 +0000

    Deploying to gh-pages from @ serkansulun/site-v2@d13965219915fd9aca4744bb72264671287855b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/site-v2/projects/";
@@ -165,51 +165,31 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/site-v2/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-audio-bandwidth-extension",
+          title: 'Audio bandwidth extension',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/site-v2/projects/bwe/";
+            },},{id: "projects-emotion-based-symbolic-music-generation",
+          title: 'Emotion-based Symbolic Music Generation',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/site-v2/projects/emotion";
+            },},{id: "projects-learned-frame-prediction-for-video-codecs",
+          title: 'Learned frame prediction for video codecs',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/site-v2/projects/lfp/";
+            },},{id: "projects-video-based-symbolic-music-generation",
+          title: 'Video-based Symbolic Music Generation',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/site-v2/projects/soundtrack/";
+            },},{id: "projects-multimodal-video-classification",
+          title: 'Multimodal video classification',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/site-v2/projects/9_project/";
+              window.location.href = "/site-v2/vemoclap";
             },},{
         id: 'social-email',
         title: 'email',
